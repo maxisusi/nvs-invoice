525abae2d003c13c4803d4a84a77deb54c91ab72
maxisusi <maxbastienbalej@gmail.com>
2022-03-22 11:23:30 +0100

    perf(clientlist): add dynamic import to ClientDetails component

--- components/clientList/ClientList.tsx
+++ components/clientList/ClientList.tsx
@@ -1,8 +1,15 @@
 import React, { FunctionComponent } from "react";
 import { DataGrid } from "@mui/x-data-grid";
+import { Props as CDProps } from "../ClientDetails/helper";
 import { Props } from "./helper";
 import { useClientList } from "./useClientList";
-import { ClientDetails } from "../ClientDetails";
+
+import dynamic from "next/dynamic";
+
+// Dynamic import of Client details for better perfomance
+const ClientDetails = dynamic<CDProps>(() =>
+  import("../ClientDetails").then((mod) => mod.ClientDetails)
+);
 
 export const ClientList: FunctionComponent<Props> = ({ clientList }) => {
   const { rows, columns, clientDetails, openClient, setOpenClient, setRows } =
@@ -12,12 +19,15 @@ export const ClientList: FunctionComponent<Props> = ({ clientList }) => {
     <>
       <div style={{ height: 500, width: "100%" }}>
         <DataGrid rows={rows} columns={columns} />
-        <ClientDetails
-          open={openClient}
-          setOpenClient={setOpenClient}
-          clientDetails={clientDetails}
-          setRows={setRows}
-        />
+
+        {openClient && (
+          <ClientDetails
+            open={openClient}
+            setOpenClient={setOpenClient}
+            clientDetails={clientDetails}
+            setRows={setRows}
+          />
+        )}
       </div>
     </>
   );
